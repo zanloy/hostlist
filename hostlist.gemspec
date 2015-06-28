22cc44d5b5940c375312f199255878ab07d12055
Zan Loy <zan.loy@sparcedge.com>
2015-06-28 10:04:52 -0400

    version bump

---
+++ hostlist.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |gem|
+  gem.name        = 'hostlist'
+  gem.version     = '0.0.2'
+  gem.licenses    = ['MIT']
+  gem.date        = '2015-06-27'
+  gem.summary     = 'Host List Generator'
+  gem.description = 'Generates list of hosts based on tags.'
+  gem.authors     = ['Zan Loy']
+  gem.email       = ['zan.loy@gmail.com']
+  gem.homepage    = 'http://sparcedge.com'
+  gem.files       = `git ls-files`.split("\n") - %w[.gitignore]
+  gem.executables = ['hostlist']
+
+  gem.add_runtime_dependency 'bracecomp', '~> 0.1', '>= 0.1.2'
+  gem.add_runtime_dependency 'daybreak', '~> 0.3', '>= 0.3.0'
+  gem.add_runtime_dependency 'thor', '~> 0.19', '>= 0.19.1'
+end
--- lib/hostlist.rb
+++ lib/hostlist.rb
@@ -1,3 +1,4 @@
+require 'bracecomp'
 require 'daybreak'
 require 'digest'
 require 'thor'
@@ -5,14 +6,20 @@ require 'yaml'
 
 class HostList < Thor
 
-  class_option :yaml, type: :string, default: 'hostlist.yaml'
+  default_task :list
+
+  class_option :yaml, type: :string, default: '/etc/hostlist.yaml'
   class_option :db, type: :string, default: '~/.hostlist.db'
+
+  def method_missing(method, *args)
+    args = ['list', method.to_s] + args
+    HostList.start(args)
+  end
 
   desc 'list', 'List hosts based on a tag.'
   def list(*tags)
     # Verify we have the most recent data in our daybreak cache.
-    db_filename = File.expand_path(options[:db])
-    db = Daybreak::DB.new db_filename
+    db = open_db
     begin
       if db.has_key? :md5
         file_md5 = Digest::MD5.file options[:yaml]
@@ -23,12 +30,16 @@ class HostList < Thor
       db.load
       # Collect all the tags from the database
       hosts = []
-      tags.each do |tag|
-        if db.has_key? tag
-          if hosts == []
-            hosts = db[tag]
-          else
-            hosts = hosts & db[tag]
+      if tags.empty?
+        hosts = db[:all]
+      else
+        tags.each do |tag|
+          if db.has_key? tag
+            if hosts == []
+              hosts = db[tag]
+            else
+              hosts = hosts & db[tag]
+            end
           end
         end
       end
@@ -38,9 +49,21 @@ class HostList < Thor
     end
   end
 
+  desc 'Show tags', 'Show all the tags in the cache database.'
+  def show
+    db = open_db
+    begin
+      db.keys.each do |key|
+        puts key
+      end
+    ensure
+      db.close
+    end
+  end
+
   desc 'print_db', 'Print the contents of the database for debugging purposes'
   def print_db
-    db = Daybreak::DB.new File.expand_path(options[:db])
+    db = open_db
     begin
       db.keys.each do |key|
         puts "db[#{key}] = #{db[key]}"
@@ -51,20 +74,28 @@ class HostList < Thor
   end
 
   no_tasks do
+    def open_db
+      Daybreak::DB.new File.expand_path(options[:db])
+    end
+
     def generate_db(db)
-      puts 'called generate_db'
       db.clear
+      db[:all] = []
       hosts = YAML.load_file(options[:yaml])
       hosts.each do |key, value|
         value['tags'].each do |tag|
           if db.has_key? tag
-            db[tag] << key
+            db[tag] += key.expand
           else
-            db[tag] = [key]
+            db[tag] = key.expand
           end
         end
+        # Add servers to :all
+        # TODO: Figure out why this isn't working. db[:all] is always an empty array.
+        key.expand.each { |host| db[:all] += [host] unless db[:all].include?(host) }
       end
       db[:md5] = Digest::MD5.file(options[:yaml]).to_s
+      db.flush
     end
   end
 
